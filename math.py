c425c06121694cf412a78f4db3aad641227ab2da
eltonLionel <drool.chad9@gmail.com>
2021-02-19 22:56:13 -0500

    ath opertations

---
+++ math.py
@@ -0,0 +1,17 @@
+a,b  = map(int,input("Enter two numbers").split())
+
+# addition
+
+print('Sum is :',a+b)
+
+# Subrtaction
+
+print('Difference is :',b-a)
+
+# Multiplication
+
+print('Multiplication is :',a*b)
+
+# Division
+
+print('Division is :',float(a)/float(b))
